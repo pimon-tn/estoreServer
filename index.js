930792712acb1a76020861e8424ba838df96707e
pornpimon <pornpimon.t@g-able.com>
2024-06-13 13:12:39 +0700

    implement estoreServer and config postgresql

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const productCategories = require('./Routes/productCategories');
+const app = express();
+
+app.use("/productCategories", productCategories);
+
+const PORT = 5001;
+const server = app.listen(PORT, ()=>{
+    console.log("App is running on the port - 5001");
+})
